0155a1e49d6264f2e3d43ddc16688f8c33303e9a
godfat <godfat@aacd4d62-77d4-4986-9b85-a1fd8b48a9cb>
2007-04-23 17:23:11 +0000

    ..........
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ludy@3 aacd4d62-77d4-4986-9b85-a1fd8b48a9cb

---
+++ test/test_z_combinator.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/ruby
+
+#    Copyright (c) 2007, Lin Jen-Shin（a.k.a. godfat 真常）
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require 'test/unit'
+require 'z_combinator'
+include Ludy
+class TestZCombinator < Test::Unit::TestCase
+  def test_z_combinator
+    fact_ = lambda{|this|
+      lambda{|n| n==1 ? 1 : n*this[n-1]}
+    }
+    fact = Z[fact_]
+    assert_equal(3628800, fact[10])
+
+    fib_ = lambda{|this|
+      lambda{|n| n<=1 ? 1 : this[n-2]+this[n-1]}
+    }
+    fib = Z[fib_]
+    assert_equal([1,1,2,3,5,8,13,21,34,55], (0...10).map(&fib))
+  end
+end
