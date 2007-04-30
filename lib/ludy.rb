bfc586ed42e6646375d0949bd2eb3ab8d4058f8d
godfat <godfat@aacd4d62-77d4-4986-9b85-a1fd8b48a9cb>
2007-04-30 10:09:35 +0000

    much path change
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ludy@7 aacd4d62-77d4-4986-9b85-a1fd8b48a9cb

--- lib/ludy.rb
+++ lib/ludy.rb
@@ -1,7 +1,31 @@
 #!/usr/bin/ruby
 
-$:.unshift File.join(File.dirname(__FILE__))
-Dir.foreach(File.dirname(__FILE__)){ |f|
-  next if f == '.' || f == '..'
-  require f
-}
+#    Copyright (c) 2007, Lin Jen-Shin¡]a.k.a. godfat ¯u±`¡^
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+def require_ludy target = nil
+  if target
+    require(File.join(File.dirname(__FILE__), 'ludy', target))
+  else
+    require_all_in_dir __FILE__, 'ludy'
+  end
+end
+
+def require_all_in_dir target_file, *dirs
+  dir = File.join(File.dirname(target_file), *dirs)
+  Dir.foreach(dir){ |f|
+    next unless f =~ /.rb$/
+    require(File.join(dir, f))
+  }
+end
