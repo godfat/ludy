14ba77ed17cd327b9800d9f6b78c04cd8ba16135
godfat <godfat@aacd4d62-77d4-4986-9b85-a1fd8b48a9cb>
2008-01-08 08:51:59 +0000

    r858@godfat-mac-ver:  godfat | 2008-01-08 14:59:28 +0800  commit the chagnes so that i can move it to ludy/kernel/defun.rb,  and extract MessageDispatcher and PatternMatcher
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ludy@65 aacd4d62-77d4-4986-9b85-a1fd8b48a9cb

--- lib/ludy/defun.rb
+++ lib/ludy/defun.rb
@@ -2,57 +2,72 @@
 require 'ludy/kernel/public_send'
 
 module Ludy
+  # you can use defun for overloading(ad-hoc polymorphism),
+  # multi-method, pattern matching, and perhaps others?
   def defun name, *args, &fun
-    p self
-    Dispatcher.create self, name, *args, &fun
+    MessageDispatcher.create self, name, *args, &fun
   end
-  class ArgList
-    def initialize *args
-      @arg_list = args
+  # someone who does the pattern matching thing
+  class PatternMatcher
+    # init for the first arg_list
+    def initialize *args; @arg_list = [args]; end
+    # find the first match of this arg_list. notice,
+    # this is not the best match. maybe someday i could
+    # implement the match policy accordingly or selectable.
+    def first_match *args
+      @arg_list.find{ |arg_list, fun|
+        true
+      }.last
     end
-    def hash
-      @arg_list.inject(0){ |r, i|
-        r + (i.kind_of?(Class) ?
-          i.object_id : i.class.object_id)
-      }
-    end
-    def eql? rhs
-      self.size == rhs.size &&
-      self.hash == rhs.hash
-    end
-    alias_method :==, :eql?
+    # delegate all the rest message to @arg_list array
     def method_missing msg, *args, &block
-      @arg_list.public_send msg, *args, &block
-    end
-  end
-  class Dispatcher
-    def self.create actor, msg, *args, &fun
-      if (@patchers ||= {})[[actor, msg]].nil?
-        @patchers[[actor, msg]] = Dispatcher.new actor, msg, *args, &fun
+      if @arg_list.respond_to? msg
+        @arg_list.public_send msg, *args, &block
       else
-        @patchers[[actor, msg]].push ArgList.new(*args), &fun
+        raise NoMethodError.new("PatternMatcher doesn't respond to #{msg}")
       end
     end
+  end
+  class MessageDispatcher
+    # create a dispatcher or append arg_list to the existed dispatcher
+    def self.create actor, msg, *args, &fun
+      if (@patchers ||= {})[[actor, msg]].nil?
+        @patchers[[actor, msg]] = MessageDispatcher.new actor, msg, *args, &fun
+      else
+        @patchers[[actor, msg]].listen *args, &fun
+      end
+    end
+    # actor is the listener who listen to the message
     def initialize actor, msg, *args, &fun
       @actor = actor
       @msg = msg
-      @map = {}
-      self.push ArgList.new(*args), &fun
-      @actor.instance_variable_set :@ludy_dispatcher_injection
+      @matcher = PatternMatcher.new *args, &fun
+      define_method_in_actor
     end
-    def push arg_list, &fun
-      @map[arg_list] = fun
-      unless @actor.respond_to? @msg
-        @actor.define_singleton_method @msg do |*args, &block|
-          # cannot see @map...
-          fun = @map[ArgList.new(*args)]
+    # open your ear and listen, and call me when you hear
+    def listen arg_list, &fun
+      @matcher << [arg_list, fun]
+    end
+    def define_method_in_actor
+      # if @actor.respond_to? @msg
+        # raise RuntimeError.new("#{@actor.inspect}'s method \"#{@msg}\" already defined.")
+      # else
+        matcher = @matcher
+        msg = @msg # make them close in the closure
+
+        @actor.define_singleton_method msg do |*args, &block|
+          fun = matcher.first_match *args
           if fun.nil?
-            @actor.method_missing @msg, *args, &block
+            if self.respond_to? :method_missing
+              self.method_missing msg, *args, &block
+            else
+              raise NoMethodError.new("#{@actor.inspect} doesn't respond to \"#{msg}\"")
+            end
           else
             fun.call(*args, &block)
           end
         end
-      end
+      # end
     end
   end
 end
