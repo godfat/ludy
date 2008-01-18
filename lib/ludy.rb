533c12ab0a19d73c1562fc3a16ab5b154011e23f
godfat <godfat@aacd4d62-77d4-4986-9b85-a1fd8b48a9cb>
2008-01-18 06:13:59 +0000

    r995@220-135-28-18:  godfat | 2008-01-18 14:13:26 +0800  * fix version number in ludy.rb....  * is there any better way to change version number?
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ludy@86 aacd4d62-77d4-4986-9b85-a1fd8b48a9cb

--- lib/ludy.rb
+++ lib/ludy.rb
@@ -12,7 +12,7 @@ require 'rake'
 module Ludy
 
   # :stopdoc:
-  VERSION = '0.1.0'
+  VERSION = '0.1.2'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   $LOAD_PATH << LIBPATH
