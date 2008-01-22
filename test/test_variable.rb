e6e6159aa2357a56b9769eb1616e808d1ffa6e87
godfat <godfat@aacd4d62-77d4-4986-9b85-a1fd8b48a9cb>
2008-01-22 12:09:01 +0000

    r1075@220-135-28-18:  godfat | 2008-01-22 20:08:36 +0800  * added swap implemention test
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ludy@91 aacd4d62-77d4-4986-9b85-a1fd8b48a9cb

--- test/test_variable.rb
+++ test/test_variable.rb
@@ -4,12 +4,22 @@ require 'ludy/variable'
 
 class TestVariable < Test::Unit::TestCase
   include Ludy
+  def swap a, b
+    a.__obj__, b.__obj__ = b.__obj__, a.__obj__
+  end
+
+  def test_swap
+    a, b = var(1), var(2)
+    swap a, b
+    assert_equal 2, a
+    assert_equal 1, b
+  end
+
   class Qoo
     def cool
       'cool ~~~~'
     end
   end
-
   def test_variable
     x = var Qoo.new
     y = x
