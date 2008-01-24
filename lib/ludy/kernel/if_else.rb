57855b33a0a5aaaecdf20da3d82c0471e2dcf76c
godfat <godfat@aacd4d62-77d4-4986-9b85-a1fd8b48a9cb>
2008-01-24 08:24:48 +0000

    r1102@Macintosh:  godfat | 2008-01-24 16:24:13 +0800  * fix a name error... in ruby 1.9-svn ?
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ludy@99 aacd4d62-77d4-4986-9b85-a1fd8b48a9cb

--- lib/ludy/kernel/if_else.rb
+++ lib/ludy/kernel/if_else.rb
@@ -8,7 +8,7 @@ module Kernel
   end
   # another else variant...
   def else
-    if self && !self.kind_of?(Blackhole) then self
+    if self && !self.kind_of?(Ludy::Blackhole) then self
     else yield end
   end
 end
