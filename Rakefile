3efd74eaaf99daa795a58f2dabfbcd1cd6349a17
godfat <godfat@aacd4d62-77d4-4986-9b85-a1fd8b48a9cb>
2008-03-01 09:43:57 +0000

    r1995@220-135-28-18:  godfat | 2008-03-01 17:43:19 +0800  * added magic comment encoding?  * don't define Proc#curry if we are using ruby 1.9  * but ruby 1.9's Proc#curry is broken on Symbol#to_proc.    it would also fail on function that produce lambda?
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ludy@139 aacd4d62-77d4-4986-9b85-a1fd8b48a9cb

--- Rakefile
+++ Rakefile
@@ -1,7 +1,7 @@
+# -*- coding: utf-8 -*-
 # Look in the tasks/setup.rb file for the various options that can be
 # configured in this Rakefile. The .rake files in the tasks directory
 # are where the options are used.
-
 load 'tasks/setup.rb'
 
 ensure_in_path 'lib'
--- lib/ludy/proc/curry.rb
+++ lib/ludy/proc/curry.rb
@@ -1,39 +1,41 @@
 
 require 'ludy/kernel/public_send'
 
-class Proc
-  def __curry__ *pre # :nodoc:
-    lambda{ |*post| self[*(pre + post)] }
-  end
+if RUBY_VERSION < '1.9.0'
+  class Proc
+    def __curry__ *pre # :nodoc:
+      lambda{ |*post| self[*(pre + post)] }
+    end
 
-  # make the caller be a curried function
-  #   lambda{|a,b,c| [a,b,c]}.curry[1][2][3]
-  #   => [1,2,3]
-  def curry
-    class << self
-      alias_method :__call__, :call
-      def call *args, &block # :nodoc:
-        if self.arity == -1
-          begin # let's try if arguments are ready
-            # is there any better way to determine this?
-            # it's hard to detect correct arity value when
-            # Symbol#to_proc happened
-            # e.g., :message_that_you_never_know.to_proc.arity => ?
-            # i'd tried put hacks in Symbol#to_proc, but it's
-            # difficult to implement in correct way
-            # i would try it again in other day
+    # make the caller be a curried function
+    #   lambda{|a,b,c| [a,b,c]}.curry[1][2][3]
+    #   => [1,2,3]
+    def curry
+      class << self
+        alias_method :__call__, :call
+        def call *args, &block # :nodoc:
+          if self.arity == -1
+            begin # let's try if arguments are ready
+              # is there any better way to determine this?
+              # it's hard to detect correct arity value when
+              # Symbol#to_proc happened
+              # e.g., :message_that_you_never_know.to_proc.arity => ?
+              # i'd tried put hacks in Symbol#to_proc, but it's
+              # difficult to implement in correct way
+              # i would try it again in other day
+              self.public_send :__call__, *args, &block
+            rescue ArgumentError # oops, let's curry it
+              method(:call).to_proc.public_send :__curry__, *args
+            end
+          elsif args.size == self.arity
             self.public_send :__call__, *args, &block
-          rescue ArgumentError # oops, let's curry it
+          else
             method(:call).to_proc.public_send :__curry__, *args
           end
-        elsif args.size == self.arity
-          self.public_send :__call__, *args, &block
-        else
-          method(:call).to_proc.public_send :__curry__, *args
         end
+        alias_method :[], :call
       end
-      alias_method :[], :call
+      self
     end
-    self
   end
 end
