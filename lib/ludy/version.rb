1399db494d69e6fcd3429201d6c14dcb4edde9e2
Lin Jen-Shin <godfat@godfat.org>
2011-02-26 17:09:58 +0800

    version.rb: quick fix Gem::Version.new(::RUBY_VERSION.dup)

--- lib/ludy/version.rb
+++ lib/ludy/version.rb
@@ -1,7 +1,7 @@
 
 require 'rubygems'
 module Ludy
-  GEM_RUBY_VERSION = Gem::Version.new(::RUBY_VERSION)
+  GEM_RUBY_VERSION = Gem::Version.new(::RUBY_VERSION.dup)
   module_function
   def ruby_before ver
     GEM_RUBY_VERSION < Gem::Version.new(ver)
